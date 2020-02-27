541f97fc0dc2c4e578ee8bc2e991268645b941ea
Scott Brenner <scott@scottbrenner.me>
2020-02-27 08:39:55 -0800

    Skip requesting account id

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 # Specify the provider and access details
 provider "aws" {
-  region = var.aws_region
+  region                     = var.aws_region
+  skip_requesting_account_id = true
 }
 
 ## Fargate
