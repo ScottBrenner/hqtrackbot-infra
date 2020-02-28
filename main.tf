fb3aa57fde0940c8f636486c186650b0053f36b4
Scott Brenner <scott@scottbrenner.me>
2020-02-27 21:43:15 -0800

    Remove stuff

--- main.tf
+++ main.tf
@@ -1,15 +1,6 @@
 # Specify the provider and access details
 provider "aws" {
   region                      = var.aws_region
-  skip_requesting_account_id  = true
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  access_key                  = "mock_access_key"
-  secret_key                  = "mock_secret_key"
-
-  endpoints {
-    sts = "https://sts.us-west-1.amazonaws.com"
-  }
 }
 
 ## Fargate
